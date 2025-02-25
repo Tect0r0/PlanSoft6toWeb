b34ed19d4573cc9faf96e4be399a6206627325a0
Gerardo Leiva Diaz <a01198479@tec.mx>
2025-02-25 11:01:04 -0600

    Starting SQL API

--- BackendActA1/controllers/index.controllers.js
+++ BackendActA1/controllers/index.controllers.js
@@ -1,2 +1,5 @@
+// Controllers are the requests that are gonna be made by the routes to the server.
+// This is the controller for the index route.
+
 export const getIndex = (req, res) => res.send("Hello, World from API!");
 export const getPing = (req, res) => res.send("Pong!");
--- BackendActA1/index.js
+++ BackendActA1/index.js
@@ -1,9 +1,12 @@
 // const express = require('express');
+import "dotenv/config";
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
+import itemsRoutes from "./routes/items.routes.js";
 
 const app = express();
 
 app.use(indexRoutes);
+app.use(itemsRoutes);
 
 app.listen(5000, console.log("http://localhost:5000"));
