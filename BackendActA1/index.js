4e3403fd884212430a8f37f07356d9defc11181b
Gerardo Leiva Diaz <a01198479@tec.mx>
2025-03-06 12:23:21 -0600

    MongoDB implemented

--- BackendActA1/index.js
+++ BackendActA1/index.js
@@ -5,15 +5,20 @@ import morgan from "morgan";
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
 import itemsRoutes from "./routes/items.routes.js";
+import items2Routes from "./routes/items2.routes.js";
 import loginRoutes from "./routes/login.routes.js";
+import { connectDB } from "./utils/mongodb.js";
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(indexRoutes);
 app.use(itemsRoutes);
+app.use(items2Routes);
 app.use(loginRoutes);
 
 app.listen(5000, console.log("http://localhost:5000"));
