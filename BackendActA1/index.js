7feb8b42e7e2e407ff98ea7f7bea2f8612239019
Gerardo Leiva Diaz <a01198479@tec.mx>
2025-05-15 12:51:57 -0600

    Intento mil ochomil

--- BackendActA1/index.js
+++ BackendActA1/index.js
@@ -1,3 +1,4 @@
+// const express = require('express');
 import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
@@ -5,12 +6,11 @@ import express from "express";
 import indexRoutes from "./routes/index.routes.js";
 import itemsFBRoutes from "./routes/itemsFB.routes.js";
 import loginRoutes from "./routes/login.routes.js";
-import serverlessExpress from "@vendia/serverless-express";
 
 const app = express();
 
 const allowedOrigins = [
-  "http://localhost:3000",
+  `http://localhost:3000`,
   "https://plan-soft6to-web.vercel.app",
 ];
 
@@ -37,5 +37,6 @@ app.use(indexRoutes);
 app.use(itemsFBRoutes);
 app.use(loginRoutes);
 
-// Export the serverless handler for Vercel
-export default serverlessExpress({ app });
+export default app;
+
+// app.listen(5000, console.log("http://localhost:5000"));
